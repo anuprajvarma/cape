1c973ef4c19fa00aef10a596e2ae7cf296c19ffa
anuprajvarma <anupraj1854@gmail.com>
2025-05-24 19:10:18 +0530

    ui: make bookmark page

--- app/bookmark/page.tsx
+++ app/bookmark/page.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import CourseCard from "../component/CourseCard";
 
 const Bookmark = () => {
-  return <div>Bookmark</div>;
+  return (
+    <div className="w-full -z-20 py-[2rem] px-4 text-black border-t border-slaty flex justify-center">
+      <div className="w-[70rem] flex flex-col gap-12">
+        <div className="flex flex-wrap gap-16 items-center justify-center">
+          <CourseCard />
+          <CourseCard />
+          <CourseCard />
+          <CourseCard />
+          <CourseCard />
+          <CourseCard />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Bookmark;
